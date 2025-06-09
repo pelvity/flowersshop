99ee9ffbf68959f91fc4fd65e9b49510786a567b
pelvity <pelvity@gmail.com>
2025-06-09 16:46:32 +0200

    chore: update application configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Suppress the specific warnings from the Supabase Realtime client
+    config.ignoreWarnings = [
+      { module: /node_modules\/@supabase\/realtime-js/ }
+    ];
+    return config;
+  }
 };
 
 export default nextConfig;
